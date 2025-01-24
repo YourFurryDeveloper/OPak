7592d152fc41beac8b01c336a1fbba68145a57c4
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-01-24 09:08:08 -0500

    Add files via upload

--- opak-installer.sh
+++ opak-installer.sh
@@ -1,4 +1,7 @@
 echo "Installing OPak..."
 wget https://raw.githubusercontent.com/YourFurryDeveloper/OPak/refs/heads/main/opak.py
+pip install requests
+sudo apt install python3-requests
+sudo chmod +x opak.py
 sudo mv opak.py /usr/local/bin/opak
 echo "OPak sucessfully installed."
